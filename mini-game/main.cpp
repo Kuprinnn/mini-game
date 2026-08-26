4443924ab40b22724a3f1161352ec12d71c437a3
Kuprinnn <229492691+Kuprinnn@users.noreply.github.com>
2026-08-27 00:15:03 +0600

    v0.0.2

--- mini-game/main.cpp
+++ mini-game/main.cpp
@@ -10,19 +10,5 @@ int main()
 {
     menu();
 
-    int* number = new int;
-    cin >> *number;
-    
-
-    switch (*number)
-    {
-    case 1:
-        buy();
-        delete number;
-        break;
-    case 2:
-        break;
-    }
-
     return 0;
 }
--- mini-game/supermarket.cpp
+++ mini-game/supermarket.cpp
@@ -1,8 +1,33 @@
 #include "supermarket.h"
 
-int milk = 0;
+int milk  = 0, milk_price  = 110;
+int water = 0, water_price = 80;
 
+void menu()
+{
+    cout << "SHOP" << endl << endl;
+    // Опция над которой работаю
+    cout << "1. buy" << endl;
+    // Скоро
+    cout << "2. inventory" << endl;
+    // Скоро
+    cout << "3. history " << endl << endl;
 
+    int* number = new int;
+    cin >> *number;
+    cout << endl;
+
+    switch (*number)
+    {
+    case 1:
+        buy();
+        delete number;
+        break;
+
+    case 2:
+        break;
+    }
+}
 
 void buy()
 {
@@ -10,41 +35,74 @@ void buy()
     cout << endl;
 
     int *number = new int;
-    cin >> *number;
+    
 
-    switch (*number)
+    while (true)
     {
-    case 1:
-        cout << "+1 milk";
+        cin >> *number;
+        cout << endl;
 
-        delete number;
-        break;
-    case 2:
-        break;
+        switch (*number)
+        {
+        case 0:
+            menu();
+            break;
+
+        case 1:
+            cout << "+1 milk" << endl << endl;
+            milk += 1;
+            list();
+            break;
+
+        case 2:
+            cout << "+1 water" << endl << endl;
+            water += 1;
+            list();
+            break;
+
+        case 3:
+            if (water == 0)
+            {
+                cout << "your basket: " << endl;
+                cout << "milk = " << milk << endl;
+                cout << "total: " << milk * milk_price + water * water_price << endl;
+            }
+            else if (milk == 0)
+            {
+                cout << "your basket: " << endl;
+                cout << "water = " << water << endl;
+                cout << "total: " << milk * milk_price + water * water_price << endl;
+            }
+            else
+            {
+                cout << "your basket: " << endl;
+                cout << "milk = " << milk << endl;
+                cout << "water = " << water << endl;
+                cout << "total: " << milk * milk_price + water * water_price << endl;
+                //delete number;
+                //oplata();
+            }
+            break;
+
+        default:
+            break;
+        }
     }
-
-
-
 }
 
 void list()
 {
-    cout << "1.milk" << endl;
-    cout << "2.water" << endl;
+    cout << "1. milk =  " << milk_price << endl;
+    cout << "2. water =  " << water_price << endl;
+    cout << "3. continue" << endl;
+    cout << "0. exit" << endl;
 }
 
-// Начальное меню
-void menu()
-{
-    cout << "SHOP" << endl << endl;
-    cout << "1.buy" << endl;
-    cout << "2.inventory" << endl;
-}
-
-void box()
-{
-    milk = +1;
-
-
-
-}
+//void oplata()
+//{
+//    int* number = new int;
+//    cin >> *number;
+//
+//
+//
+//}
